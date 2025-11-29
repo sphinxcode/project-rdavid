288c53fc90ad24958765efe607019aa4c0adab44
SAGE <ramoncarlodavid1@gmail.com>
2025-11-29 14:36:39 +0800

    Delete projects/project-tambayanapps directory

--- projects/project-tambayanapps/tambayan-frontend/src/components/ui/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Button } from './Button'
-export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './Card'
-export { default as Input } from './Input'
